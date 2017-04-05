89ce2d15fb7227aa99932e44e9e32b8da4ecdae8
DHJeff <jeff@arduino.org>
2017-04-05 15:20:44 +0800

    Added ADC Configuration

--- source/daplink/interface/main.c
+++ source/daplink/interface/main.c
@@ -41,6 +41,7 @@
 #include "DAP.h"
 #include "stm32f10x_lp_modes.h"
 #include "stm32f10x_i2c.h"
+#include "stm32f10x_adc.h"
 //CIR Library
 #include "ir_transmitter.h"
 #include "ir_receiver.h"
@@ -109,6 +110,7 @@ void disableIRIn(void);
 void IR_Timer_PWM_ConfigFreqKHZ(unsigned char Khz);
 void enableIROut(int khz);
 void disableIROut(void);
+void ADC_Configuration(void);
 
 // Timer task, set flags every 30mS and 90mS
 __task void timer_task_30mS(void)
@@ -895,4 +897,35 @@ void enableIROut(int khz)
 void disableIROut(void)
 {
 		TIM_Cmd(TIM2,DISABLE);
+}
+void ADC_Configuration(void)
+{
+		ADC_InitTypeDef ADC_InitStructure;
+		GPIO_InitTypeDef  GPIO_InitStructure;
+
+		GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AIN;
+		GPIO_InitStructure.GPIO_Pin   = VOL_DET_PIN;
+		GPIO_Init(VOL_DET_PORT, &GPIO_InitStructure);
+
+		RCC_ADCCLKConfig (RCC_PCLK2_Div6);
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+
+		ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+		ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+		ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+		ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+		ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+		ADC_InitStructure.ADC_NbrOfChannel = 7;
+
+		ADC_RegularChannelConfig(ADC1,ADC_Channel_7, 1,ADC_SampleTime_28Cycles5);
+		ADC_Init (ADC1, &ADC_InitStructure);
+		ADC_Cmd (ADC1,ENABLE);
+
+		ADC_ResetCalibration(ADC1);
+		while(ADC_GetResetCalibrationStatus(ADC1));
+		ADC_StartCalibration(ADC1);
+		while(ADC_GetCalibrationStatus(ADC1));
+
+		ADC_Cmd (ADC1,ENABLE);
+		ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 }
